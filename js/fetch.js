4a56dc8f13a36dc94f094e071db482e47b9e37db
AlexisChesneau <alexischesneau64@gmail.com>
2025-05-07 19:43:43 +0200

    version  responsive 1280px, modification du responsive section définition, section à propos, section pourquoi. Ajustement de la taille du padding de la nav

--- js/fetch.js
+++ js/fetch.js
@@ -7,9 +7,12 @@ fetch('./data/data.json')
       const div = document.createElement('div');
       div.classList.add('animal');
 
+      const contentDiv = document.createElement('div');
+      contentDiv.classList.add('animal-content');
+
       const h3 = document.createElement('h3');
       h3.textContent = animal.h3;
-      div.appendChild(h3);
+      contentDiv.appendChild(h3);
 
       const ul = document.createElement('ul');
       animal.liste.forEach(item => {
@@ -17,12 +20,13 @@ fetch('./data/data.json')
         li.textContent = item;
         ul.appendChild(li);
       });
-      div.appendChild(ul);
+      contentDiv.appendChild(ul);
+
+      div.appendChild(contentDiv);
 
       const img = document.createElement('img');
       img.src = animal.img;
       img.alt = animal.h3;
-
       div.appendChild(img);
 
       container.appendChild(div);
